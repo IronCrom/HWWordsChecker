6c45200abd059f91ded7c1af5723978ed1f351e9
IronCrom <ironcrom@yandex.ru>
2022-10-24 21:32:20 +0300

    Change WordsChecker class modified field

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,16 +3,19 @@ import java.util.List;
 import java.util.Set;
 
 public class WordsChecker {
-    protected String text;
+    // protected String text;
+    protected Set<String> textSet;
 
     public WordsChecker(String text) {
-        this.text = text;
+        Set<String> textSet = new HashSet<>(List.of(text.toLowerCase().split("\\P{IsAlphabetic}+")));
+        this.textSet = textSet;
+        //this.text = text;
     }
 
     public boolean hasWord(String word) {
-        text = text.toLowerCase();
-        Set<String> textSet = new HashSet<>();
-        textSet.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
+//        text = text.toLowerCase();
+//        Set<String> textSet = new HashSet<>();
+//        textSet.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
         word = word.toLowerCase();
         if (textSet.contains(word)) {
             System.out.println("Данный текст содержит слово " + "\"" + word + "\"!");
